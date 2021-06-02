c0d0ea225dc7d850a62a76cbb57b3932edfbb77e
Titus <82715493+CyberTitus@users.noreply.github.com>
2021-06-02 18:55:03 +0100

    Main File
    You can run this file by typing python main.py in your terminal.

---
+++ main.py
@@ -0,0 +1,23 @@
+from subprocess import check_output
+from rich.console import Console
+
+console = Console()
+datas = check_output(['netsh','wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+profiles = []
+passwords = []
+for data in datas:
+	if 'All User Profile' in data:
+		profiles.append(data.split(':')[1][1:-1])
+for profile in profiles:
+	try:
+		data = check_output(['netsh', 'wlan', 'show', 'profile', profile, 'key=clear']).decode('utf-8').split('\n')
+		for each in data:
+			if 'Key Content' in each:
+				passwords.append(each.split(':')[1][1:-1])
+	except:
+		pass
+
+for profile, password in zip(profiles, passwords):
+	console.print(f'[green]{profile}[yellow]|[blue]{password}')
+
+# CyberTitus 2021
